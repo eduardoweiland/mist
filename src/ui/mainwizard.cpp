a14ad19acda11a6c2d468d3ec869c47d28a7a784
Eduardo Weiland <eduardo@eduardoweiland.info>
2016-06-11 13:30:40 -0300

    Não permitir fechar o wizard com Esc

--- src/ui/mainwizard.cpp
+++ src/ui/mainwizard.cpp
@@ -1,7 +1,16 @@
+#include <QKeyEvent>
+
 #include "mainwizard.h"
 
 MainWizard::MainWizard(QWidget *parent) :
     QWizard(parent)
 {
     setupUi(this);
+}
+
+void MainWizard::keyPressEvent(QKeyEvent *e)
+{
+    if (e->key() != Qt::Key_Escape) {
+        QWizard::keyPressEvent(e);
+    }
 }
--- src/ui/mainwizard.h
+++ src/ui/mainwizard.h
@@ -3,12 +3,17 @@
 
 #include "ui_mainwizard.h"
 
+class QKeyEvent;
+
 class MainWizard : public QWizard, private Ui::MainWizard
 {
     Q_OBJECT
 
 public:
     explicit MainWizard(QWidget *parent = 0);
+
+protected:
+    void keyPressEvent(QKeyEvent *e);
 };
 
 #endif // MAINWIZARD_H
