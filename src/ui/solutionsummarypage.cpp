f3d58f949fe106f8e0d4f3e66339c0d5c5dfb95d
Eduardo Weiland <eduardo@eduardoweiland.info>
2016-06-20 13:49:15 -0300

    Exibição dos custos com maior precisão

--- src/ui/solutionsummarypage.cpp
+++ src/ui/solutionsummarypage.cpp
@@ -11,6 +11,6 @@ void SolutionSummaryPage::initializePage()
 {
     MainWizard *mainWizard = static_cast<MainWizard*>(wizard());
     indexesWidget->setCandidates(mainWizard->project.getSolution());
-    labelCostWithoutIndexes->setText(QString::number(mainWizard->project.getBaseTotalCost()));
-    labelCostSuggestedIndexes->setText(QString::number(mainWizard->project.getOptimizedTotalCost()));
+    labelCostWithoutIndexes->setText(QString::number(mainWizard->project.getBaseTotalCost(), 'g', 10));
+    labelCostSuggestedIndexes->setText(QString::number(mainWizard->project.getOptimizedTotalCost(), 'g', 10));
 }
